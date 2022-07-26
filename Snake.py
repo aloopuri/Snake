176fe1fbc006b813f61495ed1cd3f0766f2cc889
aloopuri <thassan577@gmail.com>
2022-07-26 22:10:12 +0100

    random generation of new apple position using key

--- Snake.py
+++ Snake.py
@@ -1,4 +1,4 @@
-import pygame, sys
+import pygame, sys, random
 
 pygame.init()
 
@@ -9,7 +9,7 @@ clock = pygame.time.Clock()
 White = (255,255,255)
 Green = (0,255,0)
 Black = (0,0,0)
-#screen.fill(White)
+Red = (255,0,0)
 
 pygame.draw.rect(screen, Black, [4, 5, 791, 590], 10)
 
@@ -22,6 +22,14 @@ lead_y = 300
 
 lead_x_change = 0
 lead_y_change = 0
+
+randAppleX = 300
+randAppleY = 300
+
+def randomApplePos():
+    newX = round(random.randrange(11, 800 - block_size - 11))#/10.0)*10.0
+    newY = round(random.randrange(11, 600 - block_size - 11))#/10.0)*10.0
+    return newX, newY
 
 while not gameQuit:
     for event in pygame.event.get():
@@ -40,14 +48,19 @@ while not gameQuit:
                 elif event.key == pygame.K_DOWN:
                     lead_x_change = 0
                     lead_y_change = block_size
+                elif event.key == pygame.K_m:       ## TO DO: remove this and add collision 
+                    randAppleX, randAppleY = randomApplePos()
 
         
     lead_x += lead_x_change
     lead_y += lead_y_change
 
     screen.fill(Black)
-
     pygame.draw.rect(screen, Green, [4, 5, 791, 590], 10)
+
+    AppleThickness = 10
+    pygame.draw.rect(screen, Red, [randAppleX, randAppleY, AppleThickness, AppleThickness])
+
     pygame.draw.rect(screen, Green, [lead_x, lead_y, 10, 10])    
     pygame.display.update()
 
